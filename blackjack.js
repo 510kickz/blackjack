83f7bf5dcfa4b77decb911026519c847549c588e
brace sproul <78114295+510kickz@users.noreply.github.com>
2021-09-17 21:55:35 -0700

    Update blackjack.js
    52 card deck
    better log descriptions of what's happening

--- blackjack.js
+++ blackjack.js
@@ -3,48 +3,77 @@
 console.log('Welcome to Casino Sproul, where you come in with stacks of cash and leave with mounds of debt.')
 
 // defines an array which houses the possible card values a player can get.
-let deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+let deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11];
+function randNum() {
+    return Math.floor(Math.random() * deck.length);
+}
 
+function dealACard() {
+    let newCard;
+    let randNumTemp = randNum();
+    return deck.splice(randNumTemp, 1)[0];
+};
 
 // defines player1 and dealer1 which will be updated throughout with the current hand value.
-let player1 = deckOfCards[Math.floor(Math.random() * deckOfCards.length)] + deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
-console.log(`Your starting hand is ${player1}`);
+let player1 = 0;
+let dealer = dealACard();
+console.log(`The dealer starts with ${dealer}.`);
 
-let dealer1 = deckOfCards[Math.floor(Math.random() * deckOfCards.length)] + deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
-console.log(`The dealer's starting hand is ${dealer1}`);
-
-// while loop to figure out if the player should hit
 while (player1 <= 17) {
-    player1 = player1 + deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
-    console.log(`Hitting... 
-Your new hand is ${player1}.`);
-}
+    let cardDealt = dealACard();
+    let cardDealt2 = dealACard();
+    if (player1 === 0) {
+        player1 += cardDealt;
+        console.log(`You start with ${cardDealt}.`);
+        player1 += cardDealt2;
+        console.log(`Your second card is ${cardDealt2}.
+Your hand starts at ${player1}.`);
+    }
+    else if (player1 === 21) {
+        console.log(`Sticking.`)
+    }
+    else if (player1 != 0) {
+            player1 += cardDealt;
+            console.log(`You are hitting...
+You have been dealt ${cardDealt}.
+Your new hand value is ${player1}.`)
+    };
+};
 
-// if statment to check if player has busted
+if (player1 >= 17) {
+    console.log(`Sticking.`)
+};
+
 if (player1 > 21) {
-    console.log(`You bust with a hand of ${player1}.`);
+    console.log(`You bust with a final hand of ${player1}.`);
     process.exit(1);
-}
+};
 
-// while loop to figure out if the dealer should hit
-while (dealer1 < player1 && player1 <= 21) {
-    dealer1 = dealer1 + deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
+while (dealer <= 17) {
+    let cardDealt = dealACard();
+    dealer += cardDealt;
     console.log(`The dealer is hitting...
-The dealer's new hand is ${dealer1}.`);
-}
+The dealer has been dealt ${cardDealt}.
+The dealer's new hand value is ${dealer}.`);
+};
 
-if (dealer1 > 21 && player1 <= 21) {
-    console.log(`The dealer has bust you win with a final hand of ${player1}.`)
-    process.exit(1);
-}
+if (dealer < player1 & player1 <= 21) {
+    let cardDealt = dealACard();
+    dealer += cardDealt;
+    console.log(`The dealer is hitting...
+The dealer has been dealt ${cardDealt}.
+The dealer's new hand value is ${dealer}.`);
+};
 
-// else if statments to check who winner is
-if (player1 > dealer1 && player1 <= 21) {
-    console.log(`You win! Your winning hand is ${player1}, while the dealer has a hand of ${dealer1}.`);
-} 
-else if (dealer1 > player1 && dealer1 <= 21) {
-    console.log(`You lose to the house. Your final hand is ${player1}, while the house had a final hand of ${dealer1}.`);
-} 
-else if (player1 === dealer1 && player1 <= 21) {
-    console.log(`Tie game. Both parties had a final hand of ${player1}.`);
+if (dealer > 21) {
+    console.log(`Dealer bust, you win with a final hand of ${player1} to ${dealer}.`);
 }
+else if (player1 > dealer) {
+    console.log(`You won! Your winning hand total is ${player1}, while the dealer has ${dealer}.`)
+} 
+else if (dealer > player1) {
+    console.log(`You lose to the house. Your final hand total is ${player1}, while the dealer has a hand of ${dealer}.`)
+} 
+else if (dealer === player1 & player1 < 22) {
+    console.log(`Tie game, you each have ${dealer}.`);
+};
